4a5fca0b05765887710e1e2174afb88e9a75c33d
chefyeum <chefyeum@gmail.com>
2021-04-28 20:33:16 +0900

    display player sprite

--- app/lib/PlayerRepr.dart
+++ app/lib/PlayerRepr.dart
@@ -3,8 +3,31 @@ import 'package:flutter/material.dart';
 class PlayerRepr extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [Text("SomePlayerName"), Text("123400")]);
+    return Column(children: [Text("SomePlayerName"), circle]);
   }
 }
+
+var circle = Center(
+  child: Container(
+    margin: EdgeInsets.all(10),
+    padding: EdgeInsets.all(10),
+    width: 90,
+    height: 45,
+    child: Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text("5020"),
+        ],
+      ),
+    ),
+    decoration: BoxDecoration(
+      border: Border.all(width: 3),
+      borderRadius: BorderRadius.all(
+        Radius.circular(200),
+      ),
+      color: Colors.yellow,
+    ),
+  ),
+);
