e36e511837416a1c1cbaf2bbd66e3e8b411032ca
chefyeum <chefyeum@gmail.com>
2021-05-06 00:41:12 +0900

    client request to sign up

--- app/lib/SignupScreen.dart
+++ app/lib/SignupScreen.dart
@@ -2,22 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-// class SignupInfo {
-//   final String username, password;
-//   SignupInfo({@required this.username, @required this.password});
-// }
-
-Future<String> fetchString() async {
-  final response = await http.get(Uri.http('localhost:8081', '/'));
-
+Future<bool> createUser(String id, name, pw) async {
+  final http.Response response = await http.post(
+    Uri.http('localhost:8081', '/user'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{'id': id, 'name': name, 'password': pw}),
+  );
   if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    return response.body;
+    return true;
   } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to request');
+    throw Exception('Failed to load album');
   }
 }
 
@@ -27,17 +23,34 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
-  Future<String> futureStr;
+  var signupRes;
+  final _formKey = GlobalKey<FormState>();
+  final Map<String, TextEditingController> _controllers = {
+    "id": TextEditingController(),
+    "name": TextEditingController(),
+    "pw": TextEditingController()
+  };
 
   @override
   void initState() {
     super.initState();
-    futureStr = fetchString();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    var status = FutureBuilder<String>(
+        future: signupRes,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Text(snapshot.data);
+          } else if (snapshot.hasError) {
+            return Text("${snapshot.error}");
+          } else {
+            return CircularProgressIndicator();
+          }
+        });
+
+    var child = Scaffold(
       appBar: AppBar(
         title: Text('Chippy'),
       ),
@@ -45,26 +58,33 @@ class _SignupScreenState extends State<SignupScreen> {
           child: Column(
         children: [
           Text("Sign up to Chippy"),
-          TextField(),
-          TextField(),
-          FutureBuilder<String>(
-              future: futureStr,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return Text(snapshot.data);
-                } else if (snapshot.hasError) {
-                  return Text("${snapshot.error}");
-                }
-
-                // By default, show a loading spinner.
-                return CircularProgressIndicator();
-              }),
+          TextField(
+            controller: _controllers["name"],
+          ),
+          TextField(
+            controller: _controllers["id"],
+          ),
+          TextField(
+            controller: _controllers["pw"],
+          ),
           ElevatedButton(
             child: Text('Register'),
-            onPressed: () {}, // TODO: add signup
-          )
+            onPressed: () {
+              signupRes = createUser(_controllers["id"].text,
+                  _controllers["name"].text, _controllers["pw"].text);
+
+              // Error handling:
+              // if (response.statusCode == 201) {
+              // } else {
+              //   throw Exception('Failed to create album.');
+              // }
+            },
+          ),
+          status
         ],
       )),
     );
+
+    return Form(key: _formKey, child: child);
   }
 }
