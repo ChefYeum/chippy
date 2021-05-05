68a7d05e11d14ccc8f69644884ca225b6d415394
Suho Lee <susemeee@gmail.com>
2021-05-05 19:02:21 +0900

    feat(server): Use file rather than memory database

--- server/node/src/lib/db.ts
+++ server/node/src/lib/db.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 const initConnection = async () => {
-  const sequelize = new Sequelize("sqlite::memory:");
+  const sequelize = new Sequelize("sqlite:./db.sqlite3");
   return sequelize;
 };
 
