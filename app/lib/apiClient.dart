5b6ac916764d92e988ca9dad555f118ce55b8249
chefyeum <chefyeum@gmail.com>
2021-05-06 01:12:26 +0900

    login request function

--- app/lib/apiClient.dart
+++ app/lib/apiClient.dart
@@ -1,6 +1,8 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+// Uri getUriForPath(String path) => Uri.http('localhost:8081', path);
+
 Future<bool> createUser(String id, name, pw) async {
   final http.Response response = await http.post(
     Uri.http('localhost:8081', '/user'),
@@ -12,6 +14,21 @@ Future<bool> createUser(String id, name, pw) async {
   if (response.statusCode == 200) {
     return true;
   } else {
-    throw Exception('Failed to load album');
+    throw Exception('Failed to sign up');
+  }
+}
+
+Future<String> login(String id, pw) async {
+  final http.Response response = await http.post(
+    Uri.http('localhost:8081', '/login'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{'id': id, 'password': pw}),
+  );
+  if (response.statusCode == 200) {
+    return "succ success";
+  } else {
+    throw Exception('Failed to login');
   }
 }
